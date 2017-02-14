6f1df65195b0681999d5ef7af548cc17f98d11e2
Jorge Moreno <alterebro@gmail.com>
2017-02-14 16:14:43 +0100

    adding analytics on browser environment

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+var PhoneGap = (document.URL.match(/^https?:/) === null);
 
 function getData() {
 
@@ -187,4 +188,16 @@ var router = {
 window.onload = function() {
 
 	getData();
+}
+
+
+if (!PhoneGap) {
+
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-63335-28', 'auto');
+	ga('send', 'pageview');
 }
